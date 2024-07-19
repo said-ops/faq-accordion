8dfa0924bfced3c00d227d7878dc85cde27af59d
said-ops <idrissisaid29@gmail.com>
2024-07-19 03:51:13 +0000

    loades with first answer shown

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ document.addEventListener('DOMContentLoaded', function (){
     const buttons = document.querySelectorAll('.show');
     const answers = document.querySelectorAll('.answer');
 
+    if(answers.length>0 && buttons.length>0){
+        buttons[0].style.backgroundImage ='url(./assets/images/icon-minus.svg)';
+        answers[0].style.display='block';
+    }
+
     buttons.forEach((button, index) => {
     button.addEventListener('click' , function (){
         const answer = answers[index];
